e17e6be64d7888205551f48a0a13783c1108fc90
Denise Mauldin <denisemauldin@users.noreply.github.com>
2020-08-01 16:51:35 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>Test seeing saving again!</h2>
+      <h2>Test seeing updates!</h2>
     </div>
   );
 }
